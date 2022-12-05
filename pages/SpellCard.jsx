becbc6428bf8cafa6a9e021aba84363b19f317fa
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2022-12-05 16:46:49 -0500

    Add title and description to card

--- pages/SpellCard.jsx
+++ pages/SpellCard.jsx
@@ -1,9 +1,42 @@
-
+import styles from '../styles/SpellCard.module.css' ;
+import { Chip } from '@mui/material';
 
 export default function spellCard({ spellData }) {
+    const name = spellData.name;
+    const description = spellData.desc;
+    const higherDesc = spellData.higher_level;
+    const concentration = spellData.concentration;
+    const school = spellData.school;
+    const classes = spellData.dnd_class;
+    const range = spellData.range;
+    const level = spellData.level;
+    const time = spellData.casting_time;
+
     return(
-        <section className="card-container">
-            <p>{spellData.name}</p>
+        <section className={styles.container}>
+            <section className={styles.top}>
+                <div className={styles.topLeft}>
+                    <p>{school}</p>
+                </div>
+                <div className={styles.topCenter}>
+                    <h2>{name}</h2>
+                    <p>{level} {school}</p>
+                </div>
+                <div className={styles.topRight}>
+                    <p>{classes}</p>
+                </div>
+            </section>
+            <div className={styles.tagsContainer}>
+            </div>
+            <section className={styles.descriptions}>
+                <div className={styles.description}>
+                    <p>{description}</p>
+                </div>
+                <div className={styles.higherDesc}>
+                    <p className={styles.higherLevelTitle}>At Higher Levels:</p>
+                    <p>{higherDesc}</p>
+                </div>
+            </section>
         </section>
     )
 }
